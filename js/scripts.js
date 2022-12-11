11e7b98f984337240b812eeff0e0f586b12f84d6
RKrueg <thegckid@gmail.com>
2022-12-10 22:14:20 -0800

    add user interface logic for displaying results on the page

--- js/scripts.js
+++ js/scripts.js
@@ -8,22 +8,24 @@ function Pizza(toppings = [], size) {
 
 Pizza.prototype.pizzaSizePrice = function () {
   if (this.size === "small") {
-    this.sizePrice += 5;
+    this.sizePrice = 5;
   } else if (this.size === "medium") {
-    this.sizePrice += 7;
+    this.sizePrice = 7;
   } else if (this.size === "large") {
-    this.sizePrice += 10;
+    this.sizePrice = 10;
   }
   return this.sizePrice;
 }
 
 Pizza.prototype.pizzaToppingsPrice = function () {
   if (this.toppings === "bacon") {
-    this.toppingPrice += 1;
+    this.toppingPrice = 1;
   } else if (this.toppings === "ham") {
-    this.toppingPrice += 2;
+    this.toppingPrice = 2;
   } else if (this.toppings === "pineapple") {
-    this.toppingPrice +=3;
+    this.toppingPrice = 3;
+  } else if (this.toppings === "pepperoni") {
+    this.toppingsPrice = 3;
   }
   return this.toppingPrice;
 }
@@ -36,3 +38,21 @@ Pizza.prototype.pizzaTotal = function () {
 Pizza.prototype.fullOrder = function () {
   return "Your toppings are: " + this.toppings + " " + "and your size is: " + this.size + ". " + "Your total is: " + "$" + this.totalBill + ". " + "Thank you!"
 }
+
+function handleFormSubmission (event) {
+  event.preventDefault();
+  const userToppings = document.querySelector("input[name=toppings]:checked").value;
+  const userSize = document.querySelector("input[name=size]:checked").value;
+  let newPizza = new Pizza(userToppings,userSize);
+  newPizza.pizzaSizePrice();
+  newPizza.pizzaToppingsPrice();
+  newPizza.pizzaTotal();
+  document.querySelector(".toppings-picked").innerText = newPizza.toppings;
+  document.querySelector(".size-picked").innerText = newPizza.size;
+  document.querySelector(".total-bill").innerText = newPizza.totalBill;
+  document.querySelector("div#pizza-details").removeAttribute("class");
+}
+
+window.addEventListener("load", function () {
+  this.document.querySelector("form#pizzaChoice").addEventListener("submit", handleFormSubmission);
+})
